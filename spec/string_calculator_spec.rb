65a9cafd583397352b9d0403c8fe41e25f1f3053
kinjal-20 <kinjalinani@gmail.com>
2024-05-13 14:19:15 +0530

    added test cases for two numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -12,5 +12,15 @@ RSpec.describe StringCalculator do
     it "returns 2 for '2'" do
       expect(StringCalculator.add('2')).to eq(2)
     end
-  end  
+  end
+
+  context 'two numbers' do
+    it "returns 3 for the string '1,2'" do
+      expect(StringCalculator.add('1,2')).to eq(3)
+    end
+
+    it "returns 200 for the string '100, 10'" do
+      expect(StringCalculator.add('100,10')).to eq(110)
+    end
+  end
 end
